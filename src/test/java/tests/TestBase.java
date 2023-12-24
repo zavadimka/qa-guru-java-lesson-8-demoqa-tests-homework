538d0cdadc263bba7fb6e9e2ccca8606a51f0aaa
Dmitrii Zavada <che.zavada@gmail.com>
2023-12-24 17:24:17 +0300

    Add attachment capability

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll(){
+
+        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.browserSize = "1920x1480";
+        Configuration.holdBrowserOpen = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        // Подключаем удалённый браузер для выполнения тестов
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        // Подключаем логгер и добавляем слушателя
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
